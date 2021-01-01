1eed50b1b86541d4712d61e93c866be0c22574d2
BlueHammy <mic1998@hotmail.co.uk>
2021-01-01 23:32:02 +0000

    Contact Form Attempt 1, Needs Work

--- src/script.js
+++ src/script.js
@@ -1,3 +1,66 @@
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function() {
+ fields.firstName = document.getElementById('firstName');
+ fields.lastName = document.getElementById('lastName');
+ fields.email = document.getElementById('email');
+})
+
+function isNotEmpty(value) {
+ if (value == null || typeof value == 'undefined' ) return false;
+ return (value.length > 0);
+}
+
+function isEmail(email) {
+  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+ return regex.test(String(email).toLowerCase());
+}
+
+function fieldValidation(field, validationFunction) {
+ if (field == null) return false;
+
+ let isFieldValid = validationFunction(field.value)
+ if (!isFieldValid) {
+ field.className = 'placeholderRed';
+ } else {
+ field.className = '';
+ }
+
+ return isFieldValid;
+}
+
+function isValid() {
+ var valid = true;
+
+ valid &= fieldValidation(fields.firstName, isNotEmpty);
+ valid &= fieldValidation(fields.lastName, isNotEmpty);
+ valid &= fieldValidation(fields.email, isEmail);
+
+ return valid;
+}
+
+
+class User {
+ constructor(firstName, lastName, email) {
+ this.firstName = firstName;
+ this.lastName = lastName;
+ this.email = email;
+ }
+}
+
+function sendContact() {
+
+ if (isValid()) {
+ let usr = new User(firstName.value, lastName.value, email.value);
+
+ alert(`${usr.firstName} thanks for registering.`)
+
+ } else {
+ alert("There was an error")
+ }
+}
+
+/*
 (function() {
     var
     // Obtain a reference to the canvas element using its id.
@@ -33,3 +96,4 @@
         redraw();
     }
 })();
+*/
